62a1f2e3db73e509b9eb2ee5701d85c438b2deb8
ruchithahoney <ruchithahoney1@gmail.com>
2023-06-06 18:53:42 -0400

    create Decison tree model

---
+++ ruchitha_model_v2.py
@@ -0,0 +1,72 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+#Import models from scikit learn module:
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold   #For K-fold cross validation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import metrics
+
+def classification_model(model, data, predictors, outcome):
+  #Fit the model:
+    model.fit(data[predictors],data[outcome])
+    predictions = model.predict(data[predictors])
+  
+    #Print accuracy
+    accuracy = metrics.accuracy_score(predictions,data[outcome])
+    print("Accuracy : %s" % "{0:.3%}".format(accuracy))
+
+    #Perform k-fold cross-validation with 5 folds
+    kf = KFold()
+    error = []
+    for train, test in kf.split(data):
+    # Filter training data
+        train_predictors = (data[predictors].iloc[train,:])
+
+    # The target we're using to train the algorithm.
+        train_target = data[outcome].iloc[train]
+
+    # Training the algorithm using the predictors and target.
+        model.fit(train_predictors, train_target)
+
+    #Record error from each cross-validation run
+        error.append(model.score(data[predictors].iloc[test,:], data[outcome].iloc[test]))
+
+        print("Cross-Validation Score : %s" % "{0:.3%}".format(np.mean(error)))
+
+    #Fit the model again so that it can be refered outside the function:
+    model.fit(data[predictors],data[outcome])
+
+df = pd.read_csv("breast_cancer.csv",header = 0)
+df.head()
+
+# clean data
+df.drop('id',axis=1,inplace=True)
+df.drop('Unnamed: 32',axis=1,inplace=True)
+
+
+df['diagnosis'] = df['diagnosis'].map({'M':1,'B':0})
+
+
+
+features_mean=list(df.columns[1:11])
+# split dataframe into two based on diagnosis
+dfM=df[df['diagnosis'] ==1]
+dfB=df[df['diagnosis'] ==0]
+
+traindf, testdf = train_test_split(df, test_size = 0.3)
+
+
+
+
+predictor_var = ['radius_mean','perimeter_mean','area_mean','compactness_mean','concave points_mean']
+outcome_var='diagnosis'
+model = DecisionTreeClassifier()
+classification_model(model,traindf,predictor_var,outcome_var)
+
+predictor_var = ['radius_mean']
+model = DecisionTreeClassifier()
+classification_model(model,traindf,predictor_var,outcome_var)
